0d0e2eb990c71f57dc006236ead92ad4aa43c5a3
Jean-Yves Delmotte <jean-yves.delmotte@greencom-networks.com>
2016-01-08 15:03:37 +0100

    MERGE FUCKING CONFLICT (act 2) merci Jeremy ROUMEGUE le gros ****

--- main-server/index.js
+++ main-server/index.js
@@ -7,12 +7,8 @@ app.use(bodyParser.json());
 app.use(cors());
 
 var router = require('./modules/router')(express.Router());
-var sphero = require('./modules/sphero/')(express.Router());
-var myo = require('./modules/myo/')(express.Router());
 
 app.use('/',router);
-app.use('/sphero', sphero);
-app.use('/myo', myo);
 console.log("Server Launched on port 3000...");
 var server = app.listen(3000);
 
