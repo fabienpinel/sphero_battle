665ce20810d2973744c0b5a8a795bc541d7769c3
Jean-Yves Delmotte <jean-yves.delmotte@greencom-networks.com>
2016-01-08 14:08:38 +0100

    joystick + servers

--- main-server/modules/router/index.js
+++ main-server/modules/router/index.js
@@ -2,11 +2,16 @@ var callbacks = require('../callbacks');
 
 module.exports = function (router) {
 
-    router.get('/hello-world', function (req, res) {
-        callbacks.helloWorld(req, res);
-    });
+    router.get('/spheros', callbacks.getSpheros);
+    router.post('/spheros', callbacks.registerSphero);
+    router.delete('/spheros/:id', callbacks.deleteSphero);
 
+    router.post('/player', callbacks.associateSpheroToPlayer);
+    router.delete('/player/:id', callbacks.deletePlayer);
+
+    router.post('/spheros/:id/color/:color', callbacks.changeSpheroColor);
+    router.post('/spheros/:id/move/:angle/:distance', callbacks.moveSphero);
+    router.post('/spheros/:id/collision', callbacks.collision);
 
     return router;
-
 };
--- sphero-server/index.js
+++ sphero-server/index.js
@@ -2,39 +2,80 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var spheroConnect = require('./modules/connect');
+var request = require('request');
 
-if (process.argv.length == 4) {
-    var app = express();
-    app.use(bodyParser.json());
-    app.use(cors());
+var server = null;
 
-    spheroConnect(process.argv[2], function (orb) {
+if (process.argv.length == 6) {
 
-        orb.color("green");
-        orb.detectCollisions();
-        orb.on("collision", function () {
-            console.log('collision');
-            orb.color("red");
-            orb.stop();
-            setTimeout(function () {
-                orb.color("green");
-            }, 1000);
-        });
+    buildSphero();
 
-        var router = require('./modules/router')(express.Router(), orb);
-        app.use('/',router);
-        console.log("Server Launched on port 8080...");
-        var server = app.listen(8080);
-
-        var io = require('socket.io').listen(server);
-        var sockets = require('./modules/sockets');
-
-        io.on('connection', function (socket) {
-            sockets(socket, orb);
-        });
-
-    });
 } else {
-    console.error('You must specify the id of the Sphero and the url of the global server\nnode index.js [sphero_id] [server_url]');
+    console.error(
+        'You must specify the id of the Sphero and the url of the global server\n' +
+        'node index.js [sphero_id] [server_url] [your_ip] [your_port]'
+    );
 }
 
+function buildSphero() {
+    function _buildSphero() {
+        var app = express();
+        app.use(bodyParser.json());
+        app.use(cors());
+
+        spheroConnect(process.argv[2], function (orb) {
+
+            request.post({
+                url: process.argv[3] + '/spheros',
+                body: {
+                    id: process.argv[2],
+                    url: 'http://' + process.argv[4] + ':' + process.argv[5]
+                },
+                json: true
+            });
+
+            orb.color("green");
+            orb.detectCollisions();
+            orb.on("collision", function () {
+                // call main server
+                request.post(process.argv[3] + '/spheros/' + process.argv[2] + '/collision');
+
+                console.log('collision');
+                orb.color("red");
+                orb.stop();
+                setTimeout(function () {
+                    orb.color("green");
+                }, 1000);
+            });
+
+            var router = require('./modules/router')(express.Router(), orb);
+            app.use('/',router);
+            console.log("Server Launched on port " + process.argv[5] + " ...");
+            server = app.listen(process.argv[5], function () {
+                recursivePing(orb);
+            });
+
+        });
+    }
+    if (server) {
+        server.close(function () {
+            server = null;
+            _buildSphero();
+        });
+    } else {
+        _buildSphero();
+    }
+}
+
+function recursivePing(orb) {
+    setTimeout(function () {
+        orb.ping(function (err) {
+            if (err) {
+                console.log('disconnect : try to reconnect');
+                //request.del(process.argv[3] + '/spheros/' + process.argv[2]);
+            } else {
+                recursivePing(orb);
+            }
+        });
+    }, 1000);
+}
