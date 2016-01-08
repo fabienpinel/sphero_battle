b3c1f9e9fc38055ba8772995ffe0abc30fd2815c
Fabien Pinel <fabienpinel@yahoo.fr>
2016-01-08 10:00:33 +0100

    everything

---
+++ cordova_app/platforms/android/com.tribalyte.plugin.myo/myoAndSphero-myo-android-sdk/build/generated/source/buildConfig/debug/com/thalmic/myo2/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.thalmic.myo2;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.thalmic.myo2";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 10000;
+  public static final String VERSION_NAME = "";
+}
