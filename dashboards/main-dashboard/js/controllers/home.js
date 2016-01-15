90f333c4986088ae073e2271f5b711a6c757b735
jeremy <roumegue.jeremy@gmail.com>
2016-01-15 16:17:31 +0100

    Dashboard avancment

--- dashboards/main-dashboard/js/controllers/home.js
+++ dashboards/main-dashboard/js/controllers/home.js
@@ -1,4 +1,6 @@
 app.controller('homeCtrl', ['$scope','$rootScope', '$location', 'User', function ($scope, $rootScope, $location, User) {
+
+    $scope.status = '  ';
     $scope.players = [];
     $scope.spheros = [];
 
@@ -19,4 +21,32 @@ app.controller('homeCtrl', ['$scope','$rootScope', '$location', 'User', function
         if ( !self.activated ) self.modes = [ ];
         if (  self.activated ) j = counter = 0;
     };
+
+    $scope.showAlert = function(ev) {
+        // Appending dialog to document.body to cover sidenav in docs app
+        // Modal dialogs should fully cover application
+        // to prevent interaction outside of dialog
+       /* $mdDialog.show(
+            $mdDialog.alert()
+                .parent(angular.element(document.querySelector('#popupContainer')))
+                .clickOutsideToClose(true)
+                .title('This is an alert title')
+                .textContent('You can specify some description text in here.')
+                .ariaLabel('Alert Dialog Demo')
+                .ok('Got it!')
+                .targetEvent(ev)
+        );*/
+    };
+
+    function DialogController($scope, $mdDialog) {
+        $scope.hide = function() {
+            $mdDialog.hide();
+        };
+        $scope.cancel = function() {
+            $mdDialog.cancel();
+        };
+        $scope.answer = function(answer) {
+            $mdDialog.hide(answer);
+        };
+    }
 }]);
