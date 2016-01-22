9964d8321facb6fb16916bddc8f44e4cd8799011
jeremy <roumegue.jeremy@gmail.com>
2016-01-22 16:56:09 +0100

    ajout des effets WOUAHHHOU !!

--- dashboards/main-dashboard/js/controllers/home.js
+++ dashboards/main-dashboard/js/controllers/home.js
@@ -1,6 +1,12 @@
 app.controller('homeCtrl', ['$scope','playersFactory','spherosFactory', 'socket', function ($scope,playersFactory,spherosFactory,socket) {
 
     var vm = this;
+    vm.init = true;
+    vm.collision1 = vm.collision2 = false;
+
+    $timeout(function(){
+        vm.init = false;
+    }, 2000);
 
     vm.players = playersFactory.getPlayers;
     vm.spheros = spherosFactory.getSpheros;
@@ -9,6 +15,23 @@ app.controller('homeCtrl', ['$scope','playersFactory','spherosFactory', 'socket'
         vm.spheros = data.spheros;
         vm.players = data.players;
     });
+
+    socket.on('collision', function (sphero) {
+        console.log('collision', sphero);
+        for (var i in vm.players) {
+            if (vm.players[i].id == sphero.playerId) {
+                makeCollision(i);
+                break;
+            }
+        }
+    });
+
+    function makeCollision(index) {
+        vm['collision' + (index+1)]  = true;
+        $timeout(function(){
+            vm['collision' + (index+1)]  = false;
+        }, 300);
+    }
 
     vm.up = function(index) {
         playersFactory.upPower(index);
