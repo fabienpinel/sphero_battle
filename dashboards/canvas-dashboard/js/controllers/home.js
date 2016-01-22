494693b6858d252e5b6a9627a64bcaba91ad8fce
jeremy <roumegue.jeremy@gmail.com>
2016-01-22 10:07:13 +0100

    New dashboard with AWESOME canvas in

---
+++ dashboards/canvas-dashboard/js/controllers/home.js
@@ -0,0 +1,18 @@
+app.controller('homeCtrl', ['$scope','playersFactory','spherosFactory', 'socket', function ($scope,playersFactory,spherosFactory,socket) {
+
+    var vm = this;
+
+    vm.players = playersFactory.getPlayers;
+    vm.spheros = spherosFactory.getSpheros;
+
+    socket.on('dataChange', function (data) {
+        vm.spheros = data.spheros;
+        vm.players = data.players;
+    });
+
+    vm.up = function(index) {
+        playersFactory.upPower(index);
+        console.log(playersFactory.upPower);
+    }
+
+}]);
