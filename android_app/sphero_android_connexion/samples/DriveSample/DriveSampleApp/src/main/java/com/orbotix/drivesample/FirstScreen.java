a98fc0cbb3cec42d9a330664d51ea2fcceb36ae6
Fabien Pinel <fabienpinel@yahoo.fr>
2016-01-29 10:25:33 +0100

    first screen activity welcome

---
+++ android_app/sphero_android_connexion/samples/DriveSample/DriveSampleApp/src/main/java/com/orbotix/drivesample/FirstScreen.java
@@ -0,0 +1,14 @@
+package com.orbotix.drivesample;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class FirstScreen extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_screen);
+    }
+
+}
