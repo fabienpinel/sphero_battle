825ada818906827bf8c8f2d4c1ddc6fdd49c0975
Fabien Pinel <fabienpinel@yahoo.fr>
2016-01-29 15:24:49 +0100

    one part of the big refactor in Connexion.java

--- android_app/sphero_android_connexion/samples/DriveSample/DriveSampleApp/src/main/java/com/orbotix/drivesample/Connexion.java
+++ android_app/sphero_android_connexion/samples/DriveSample/DriveSampleApp/src/main/java/com/orbotix/drivesample/Connexion.java
@@ -6,79 +6,60 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
 import android.widget.Button;
 
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+import com.orbotix.ConvenienceRobot;
+import com.orbotix.Ollie;
+import com.orbotix.Sphero;
+import com.orbotix.classic.DiscoveryAgentClassic;
+import com.orbotix.classic.RobotClassic;
+import com.orbotix.common.DiscoveryAgent;
+import com.orbotix.common.DiscoveryAgentEventListener;
+import com.orbotix.common.DiscoveryException;
+import com.orbotix.common.ResponseListener;
+import com.orbotix.common.Robot;
+import com.orbotix.common.RobotChangedStateListener;
+import com.orbotix.common.internal.AsyncMessage;
+import com.orbotix.common.internal.DeviceResponse;
+import com.orbotix.le.DiscoveryAgentLE;
+import com.orbotix.le.RobotLE;
+import com.orbotix.robotpicker.RobotPickerDialog;
+
+import java.net.URISyntaxException;
+import java.util.List;
+
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class Connexion extends Activity {
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
+public class Connexion extends Activity implements RobotPickerDialog.RobotPickerListener, DiscoveryAgentEventListener, RobotChangedStateListener {
 
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
 
-    /**
-     * Some older devices needs a small delay between UI widget updates
-     * and a change of the status and navigation bar.
-     */
-    private static final int UI_ANIMATION_DELAY = 300;
-    private final Handler mHideHandler = new Handler();
-    private final Runnable mHidePart2Runnable = new Runnable() {
-        @SuppressLint("InlinedApi")
-        @Override
-        public void run() {
-            // Delayed removal of status and navigation bar
+    private static final String TAG = "Connexion View";
+    private DiscoveryAgent _currentDiscoveryAgent;
 
-            // Note that some of these constants are new as of API 16 (Jelly Bean)
-            // and API 19 (KitKat). It is safe to use them, as they are inlined
-            // at compile-time and do nothing on earlier devices.
+    private RobotPickerDialog _robotPickerDialog;
 
-        }
-    };
-    private View mControlsView;
-    private final Runnable mShowPart2Runnable = new Runnable() {
-        @Override
-        public void run() {
-            // Delayed display of UI elements
-
-            mControlsView.setVisibility(View.VISIBLE);
-        }
-    };
-    private boolean mVisible;
-    private final Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            hide();
-        }
-    };
+    private ConvenienceRobot _connectedRobot;
+    private Socket mSocket;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
+        //hide the actionbar
         ActionBar actionBar = getActionBar();
         actionBar.hide();
 
-
-        mVisible = false;
         setContentView(R.layout.activity_connexion);
-
-
-
 
         final Button connectSpheroButton = (Button) findViewById(R.id.connectSpheroButton);
         connectSpheroButton.setOnClickListener(new View.OnClickListener() {
@@ -98,80 +79,190 @@ public class Connexion extends Activity {
             }
         });
 
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Create a robot picker dialog, this allows the user to select which robot they would like to connect to.
+        // We don't need to do this step if we know which robot we want to talk to, and don't need the user to
+        // decide that.
+        if (_robotPickerDialog == null) {
+            _robotPickerDialog = new RobotPickerDialog(this, this);
+        }
+        // Show the picker only if it's not showing. This kmade by Lorc under CC BY 3.0
+
+        if (!_robotPickerDialog.isShowing()) {
+            _robotPickerDialog.show();
+        }
+    }
+
+    @Override
+    protected void onPause() { super.onPause();}
+
+    @Override
+    public void onResume() {
+        super.onResume();  // Always call the superclass method first
     }
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == android.R.id.home) {
-            // This ID represents the Home or Up button.
-            NavUtils.navigateUpFromSameTask(this);
-            return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    private void toggle() {
-        if (mVisible) {
-            hide();
-        } else {
-            show();
-        }
-    }
-
-    private void hide() {
-        // Hide UI first
-        ActionBar actionBar = getActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
-        mVisible = false;
-
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.removeCallbacks(mShowPart2Runnable);
-        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    @SuppressLint("InlinedApi")
-    private void show() {
-
-        mVisible = true;
-
-        // Schedule a runnable to display UI elements after a delay
-        mHideHandler.removeCallbacks(mHidePart2Runnable);
-        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    /**
-     * Schedules a call to hide() in [delay] milliseconds, canceling any
-     * previously scheduled calls.
-     */
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
-    }
-
-
-
-
     public void connectSphero(){
+        //launch the connection with the sphero
 
     }
 
     public void connectMyo(){
+        //launch the myo connection (proximity)
+    }
 
+    @Override
+    public void handleRobotsAvailable(List<Robot> robots) {
+        // Here we need to know which version of the discovery agent we are using, if we are to use Sphero, we need to
+        // treat Spheros a little bit differently.
+        if (_currentDiscoveryAgent instanceof DiscoveryAgentClassic) {
+            // If we are using the classic discovery agent, and therefore using Sphero, we'll just connect to the first
+            // one available that we get. Note that "available" in classic means paired to the phone and turned on.
+            _currentDiscoveryAgent.connect(robots.get(0));
+        }
+        else if (_currentDiscoveryAgent instanceof DiscoveryAgentLE) {
+            // If we are using the LE discovery agent, and therefore using Ollie, there's not much we need to do here.
+            // The SDK will automatically connect to the robot that you touch the phone to, and you will get a message
+            // saying that the robot has connected.
+            // Note that this method is called very frequently and will cause your app to slow down if you log.
+        }
+    }
+
+    @Override
+    public void handleRobotChangedState(Robot robot, RobotChangedStateNotificationType type) {
+        // For the purpose of this sample, we'll only handle the connected and disconnected notifications
+        switch (type) {
+            // A robot was connected, and is ready for you to send commands to it.
+            case Online:
+                // When a robot is connected, this is a good time to stop discovery. Discovery takes a lot of system
+                // resources, and if left running, will cause your app to eat the user's battery up, and may cause
+                // your application to run slowly. To do this, use DiscoveryAgent#stopDiscovery().
+                _currentDiscoveryAgent.stopDiscovery();
+                // It is also proper form to not allow yourself to re-register for the discovery listeners, so let's
+                // unregister for the available notifications here using DiscoveryAgent#removeDiscoveryListener().
+                _currentDiscoveryAgent.removeDiscoveryListener(this);
+                // Depending on what was connected, you might want to create a wrapper that allows you to do some
+                // common functionality related to the individual robots. You can always of course use the
+                // Robot#sendCommand() method, but Ollie#drive() reads a bit better.
+                if (robot instanceof RobotLE) {
+                    _connectedRobot = new Ollie(robot);
+                }
+                else if (robot instanceof RobotClassic) {
+                    _connectedRobot = new Sphero(robot);
+                    try {
+                        _connectedRobot.enableCollisions(true);
+                        _connectedRobot.addResponseListener(new ResponseListener() {
+                            @Override
+                            public void handleResponse(DeviceResponse deviceResponse, Robot robot) {
+
+                            }
+
+                            @Override
+                            public void handleStringResponse(String s, Robot robot) {
+
+                            }
+
+                            @Override
+                            public void handleAsyncMessage(AsyncMessage asyncMessage, Robot robot) {
+                                _connectedRobot.setLed(1, 0, 0);
+                                Log.d("collision", "collision");
+                                mSocket.emit("collision");
+                                _connectedRobot.setLed(0,1,0);
+
+                            }
+                        });
+                        mSocket = IO.socket("http://134.59.215.166:3000/");
+                        //mSocket.emit("spheroId", spheroId);
+                        mSocket.connect();
+                        //mSocket.on('connection')
+                        Log.d("SPHERO ID", _connectedRobot.getRobot().getName());
+                        mSocket.emit("spheroId", _connectedRobot.getRobot().getName());
+
+                    } catch (URISyntaxException e) {
+                        Log.d("ERROR SOCKET", e.getMessage());
+                    }
+
+
+                }
+
+                // Finally for visual feedback let's turn the robot green saying that it's been connected
+                _connectedRobot.setLed(0f, 1f, 0f);
+
+                break;
+            case Disconnected:
+                    startDiscovery();
+                break;
+            default:
+                Log.v(TAG, "Not handling state change notification: " + type);
+                break;
+        }
+    }
+
+    @Override
+    public void onRobotPicked(RobotPickerDialog.RobotPicked robotPicked) {
+        // Dismiss the robot picker so that the user doesn't keep clicking it and trying to start
+        // discovery multiple times
+        _robotPickerDialog.dismiss();
+        switch (robotPicked) {
+            // If the user picked a Sphero, you want to start the Bluetooth Classic discovery agent, as that is the
+            // protocol that Sphero talks over. This will allow us to find a Sphero and connect to it.
+            case Sphero:
+                // To get to the classic discovery agent, you use DiscoveryAgentClassic.getInstance()
+                _currentDiscoveryAgent = DiscoveryAgentClassic.getInstance();
+                break;
+            // If the user picked an Ollie, you want to start the Bluetooth LE discovery agent, as that is the protocol
+            // that Ollie talks over. This will allow you to find an Ollie and connect to it.
+            case Ollie:
+                // To get to the LE discovery agent, you use DiscoveryAgentLE.getInstance()
+                _currentDiscoveryAgent = DiscoveryAgentLE.getInstance();
+                break;
+        }
+
+        // Now that we have a discovery agent, we will start discovery on it using the method defined below
+        startDiscovery();
+    }
+
+    /*
+    *
+        To pass:
+                intent.putExtra("MyClass", obj);
+        // To retrieve object in second Activity
+        getIntent().getSerializableExtra("MyClass");
+    *
+    * */
+    private void startDiscovery() {
+        try {
+            // You first need to set up so that the discovery agent will notify you when it finds robots.
+            // To do this, you need to implement the DiscoveryAgentEventListener interface (or declare
+            // it anonymously) and then register it on the discovery agent with DiscoveryAgent#addDiscoveryListener()
+            _currentDiscoveryAgent.addDiscoveryListener(this);
+            // Second, you need to make sure that you are notified when a robot changes state. To do this,
+            // implement RobotChangedStateListener (or declare it anonymously) and use
+            // DiscoveryAgent#addRobotStateListener()
+            _currentDiscoveryAgent.addRobotStateListener(this);
+            // Then to start looking for a Sphero, you use DiscoveryAgent#startDiscovery()
+            // You do need to handle the discovery exception. This can occur in cases where the user has
+            // Bluetooth off, or when the discovery cannot be started for some other reason.
+            _currentDiscoveryAgent.startDiscovery(this);
+        } catch (DiscoveryException e) {
+            Log.e(TAG, "Could not start discovery. Reason: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
--- android_app/sphero_android_connexion/samples/DriveSample/DriveSampleApp/src/main/java/com/orbotix/drivesample/MainActivity.java
+++ android_app/sphero_android_connexion/samples/DriveSample/DriveSampleApp/src/main/java/com/orbotix/drivesample/MainActivity.java
@@ -37,52 +37,19 @@ import com.github.nkzawa.socketio.client.Socket;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MainActivity extends Activity implements RobotPickerDialog.RobotPickerListener,
-                                                      DiscoveryAgentEventListener,
-                                                      RobotChangedStateListener {
+public class MainActivity extends Activity implements RobotChangedStateListener {
 
     private static final String TAG = "MainActivity";
 
-    /**
-     * Our current discovery agent that we will use to find robots of a certain protocol
-     */
-    private DiscoveryAgent _currentDiscoveryAgent;
-
-    /**
-     * The dialog that will allow the user to chose which type of robot to connect to
-     */
-    private RobotPickerDialog _robotPickerDialog;
-
-    /**
-     * The joystick that we will use to send roll commands to the robot
-     */
     private JoystickView _joystick;
 
-    /**
-     * The connected robot
-     */
     private ConvenienceRobot _connectedRobot;
 
-    /**
-     * The calibration view, used for setting the default heading of the robot
-     */
     private CalibrationView _calibrationView;
 
-    /**
-     * A button used for one finger calibration
-     */
     private CalibrationImageButtonView _calibrationButtonView;
 
-    /**
-     * The fragment to show that contains the color picker
-     */
     private ColorPickerFragment _colorPicker;
-
-
-    /**
-     * Reference to the layout containing the developer mode switch and label
-     */
-    private LinearLayout _developerModeLayout;
 
     private Socket mSocket;
     private String spheroId;
@@ -97,12 +64,6 @@ public class MainActivity extends Activity implements RobotPickerDialog.RobotPic
         setupCalibration();
         setupColorPicker();
 
-
-
-        // Here, you need to route all the touch events to the joystick and calibration view so that they know about
-        // them. To do this, you need a way to reference the view (in this case, the id "entire_view") and attach
-        // an onTouchListener which in this case is declared anonymously and invokes the
-        // Controller#interpretMotionEvent() method on the joystick and the calibration view.
         findViewById(R.id.entire_view).setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
@@ -114,75 +75,13 @@ public class MainActivity extends Activity implements RobotPickerDialog.RobotPic
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        // Create a robot picker dialog, this allows the user to select which robot they would like to connect to.
-        // We don't need to do this step if we know which robot we want to talk to, and don't need the user to
-        // decide that.
-        if (_robotPickerDialog == null) {
-            _robotPickerDialog = new RobotPickerDialog(this, this);
-        }
-        // Show the picker only if it's not showing. This kmade by Lorc under CC BY 3.0
-
-        if (!_robotPickerDialog.isShowing()) {
-            _robotPickerDialog.show();
-        }
-    }
+    protected void onStart() {  super.onStart();    }
 
     @Override
-    protected void onPause() {
-        super.onPause();
+    protected void onPause() {  super.onPause();    }
 
-    }
-
-    /**
-     * Invoked when the user makes a selection on which robot they would like to use.
-     * @param robotPicked The type of the robot that was selected
-     */
     @Override
-    public void onRobotPicked(RobotPickerDialog.RobotPicked robotPicked) {
-        // Dismiss the robot picker so that the user doesn't keep clicking it and trying to start
-        // discovery multiple times
-        _robotPickerDialog.dismiss();
-        switch (robotPicked) {
-            // If the user picked a Sphero, you want to start the Bluetooth Classic discovery agent, as that is the
-            // protocol that Sphero talks over. This will allow us to find a Sphero and connect to it.
-            case Sphero:
-                // To get to the classic discovery agent, you use DiscoveryAgentClassic.getInstance()
-                _currentDiscoveryAgent = DiscoveryAgentClassic.getInstance();
-                break;
-            // If the user picked an Ollie, you want to start the Bluetooth LE discovery agent, as that is the protocol
-            // that Ollie talks over. This will allow you to find an Ollie and connect to it.
-            case Ollie:
-                // To get to the LE discovery agent, you use DiscoveryAgentLE.getInstance()
-                _currentDiscoveryAgent = DiscoveryAgentLE.getInstance();
-                break;
-        }
-
-        // Now that we have a discovery agent, we will start discovery on it using the method defined below
-        startDiscovery();
-    }
-
-    /**
-     * Invoked when the discovery agent finds a new available robot, or updates and already available robot
-     * @param robots The list of all robots, connected or not, known to the discovery agent currently
-     */
-    @Override
-    public void handleRobotsAvailable(List<Robot> robots) {
-        // Here we need to know which version of the discovery agent we are using, if we are to use Sphero, we need to
-        // treat Spheros a little bit differently.
-        if (_currentDiscoveryAgent instanceof DiscoveryAgentClassic) {
-            // If we are using the classic discovery agent, and therefore using Sphero, we'll just connect to the first
-            // one available that we get. Note that "available" in classic means paired to the phone and turned on.
-            _currentDiscoveryAgent.connect(robots.get(0));
-        }
-        else if (_currentDiscoveryAgent instanceof DiscoveryAgentLE) {
-            // If we are using the LE discovery agent, and therefore using Ollie, there's not much we need to do here.
-            // The SDK will automatically connect to the robot that you touch the phone to, and you will get a message
-            // saying that the robot has connected.
-            // Note that this method is called very frequently and will cause your app to slow down if you log.
-        }
-    }
+    public void onResume() {    super.onResume();   }
 
     /**
      * Invoked when a robot changes state. For example, when a robot connects or disconnects.
@@ -195,14 +94,6 @@ public class MainActivity extends Activity implements RobotPickerDialog.RobotPic
         switch (type) {
             // A robot was connected, and is ready for you to send commands to it.
             case Online:
-                // When a robot is connected, this is a good time to stop discovery. Discovery takes a lot of system
-                // resources, and if left running, will cause your app to eat the user's battery up, and may cause
-                // your application to run slowly. To do this, use DiscoveryAgent#stopDiscovery().
-                _currentDiscoveryAgent.stopDiscovery();
-                // It is also proper form to not allow yourself to re-register for the discovery listeners, so let's
-                // unregister for the available notifications here using DiscoveryAgent#removeDiscoveryListener().
-                _currentDiscoveryAgent.removeDiscoveryListener(this);
-                // Don't forget to turn on UI elements
                 _joystick.setEnabled(true);
                 _calibrationView.setEnabled(true);
                 _calibrationButtonView.setEnabled(true);
@@ -215,7 +106,7 @@ public class MainActivity extends Activity implements RobotPickerDialog.RobotPic
                 }
                 else if (robot instanceof RobotClassic) {
                     _connectedRobot = new Sphero(robot);
-                    try {
+
                         _connectedRobot.enableCollisions(true);
                         _connectedRobot.addResponseListener(new ResponseListener() {
                             @Override
@@ -237,17 +128,14 @@ public class MainActivity extends Activity implements RobotPickerDialog.RobotPic
 
                             }
                         });
-                        mSocket = IO.socket("http://134.59.215.166:3000/");
+                        //mSocket = IO.socket("http://134.59.215.166:3000/");
                         //mSocket.emit("spheroId", spheroId);
                         mSocket.on("command", onNewMessage);
-                        mSocket.connect();
+                        //mSocket.connect();
                         //mSocket.on('connection')
                         Log.d("SPHERO ID", _connectedRobot.getRobot().getName());
                         mSocket.emit("spheroId", _connectedRobot.getRobot().getName());
 
-                    } catch (URISyntaxException e) {
-                        Log.d("ERROR SOCKET", e.getMessage());
-                    }
 
 
                 }
@@ -257,24 +145,11 @@ public class MainActivity extends Activity implements RobotPickerDialog.RobotPic
 
                 break;
             case Disconnected:
-                // When a robot disconnects, it is a good idea to disable UI elements that send commands so that you
-                // do not have to handle the user continuing to use them while the robot is not connected
+                //what happenned ?
+
                 _joystick.setEnabled(false);
                 _calibrationView.setEnabled(false);
                 _calibrationButtonView.setEnabled(false);
-
-                // Disable the developer mode button when the robot disconnects so that it can be set up if a LE robot
-                // connectes again
-                if (robot instanceof RobotLE && _developerModeLayout != null) {
-                    _developerModeLayout.setVisibility(View.INVISIBLE);
-                }
-
-                // When a robot disconnects, you might want to start discovery so that you can reconnect to a robot.
-                // Starting discovery on disconnect however can cause unintended side effects like connecting to
-                // a robot with the application closed. You should think carefully of when to start and stop discovery.
-                // In this case, we will not start discovery when the robot disconnects. You can uncomment the following line of
-                // code to see the start discovery on disconnection in action.
-//                startDiscovery();
                 break;
             default:
                 Log.v(TAG, "Not handling state change notification: " + type);
@@ -405,28 +280,6 @@ public class MainActivity extends Activity implements RobotPickerDialog.RobotPic
 
     }
 
-    /**
-     * Starts discovery on the set discovery agent and look for robots
-     */
-    private void startDiscovery() {
-        try {
-            // You first need to set up so that the discovery agent will notify you when it finds robots.
-            // To do this, you need to implement the DiscoveryAgentEventListener interface (or declare
-            // it anonymously) and then register it on the discovery agent with DiscoveryAgent#addDiscoveryListener()
-            _currentDiscoveryAgent.addDiscoveryListener(this);
-            // Second, you need to make sure that you are notified when a robot changes state. To do this,
-            // implement RobotChangedStateListener (or declare it anonymously) and use
-            // DiscoveryAgent#addRobotStateListener()
-            _currentDiscoveryAgent.addRobotStateListener(this);
-            // Then to start looking for a Sphero, you use DiscoveryAgent#startDiscovery()
-            // You do need to handle the discovery exception. This can occur in cases where the user has
-            // Bluetooth off, or when the discovery cannot be started for some other reason.
-            _currentDiscoveryAgent.startDiscovery(this);
-        } catch (DiscoveryException e) {
-            Log.e(TAG, "Could not start discovery. Reason: " + e.getMessage());
-            e.printStackTrace();
-        }
-    }
 
     private void myoCommandReceived(int angle, int speed){
         Log.d("command reveived", "Angle: " + angle + " ,Speed: " + speed);
@@ -457,19 +310,10 @@ public class MainActivity extends Activity implements RobotPickerDialog.RobotPic
                             _joystick.sendDataToMyo(x, y);
 
                         }
-
-
                         // add the message to view
                         //addMessage(username, message);
                     }
                 });
         }
     };
-
-    @Override
-    public void onResume() {
-        super.onResume();  // Always call the superclass method first
-    }
-
-
 }
