cde0970da77df1f33ab4a24827df1e87a5ea9fe4
Fabien Pinel <fabienpinel@yahoo.fr>
2016-01-29 14:53:43 +0100

    vue connexion et boutons connectmyo et sphero

--- android_app/sphero_android_connexion/samples/DriveSample/DriveSampleApp/src/main/java/com/orbotix/drivesample/Connexion.java
+++ android_app/sphero_android_connexion/samples/DriveSample/DriveSampleApp/src/main/java/com/orbotix/drivesample/Connexion.java
@@ -3,12 +3,14 @@ package com.orbotix.drivesample;
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
+import android.widget.Button;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -33,7 +35,6 @@ public class Connexion extends Activity {
      */
     private static final int UI_ANIMATION_DELAY = 300;
     private final Handler mHideHandler = new Handler();
-    private View mContentView;
     private final Runnable mHidePart2Runnable = new Runnable() {
         @SuppressLint("InlinedApi")
         @Override
@@ -43,12 +44,7 @@ public class Connexion extends Activity {
             // Note that some of these constants are new as of API 16 (Jelly Bean)
             // and API 19 (KitKat). It is safe to use them, as they are inlined
             // at compile-time and do nothing on earlier devices.
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
         }
     };
     private View mControlsView;
@@ -56,10 +52,7 @@ public class Connexion extends Activity {
         @Override
         public void run() {
             // Delayed display of UI elements
-            ActionBar actionBar = getActionBar();
-            if (actionBar != null) {
-                actionBar.show();
-            }
+
             mControlsView.setVisibility(View.VISIBLE);
         }
     };
@@ -70,39 +63,38 @@ public class Connexion extends Activity {
             hide();
         }
     };
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            return false;
-        }
-    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_connexion);
+
         ActionBar actionBar = getActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
-
-        mVisible = true;
+        actionBar.hide();
 
 
-        // Set up the user interaction to manually show or hide the system UI.
-        mContentView.setOnClickListener(new View.OnClickListener() {
+        mVisible = false;
+        setContentView(R.layout.activity_connexion);
+
+
+
+
+        final Button connectSpheroButton = (Button) findViewById(R.id.connectSpheroButton);
+        connectSpheroButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                toggle();
+            public void onClick(View v) {
+                Intent intent = new Intent(Connexion.this, Connexion.class);
+                startActivity(intent);
+            }
+        });
+
+        final Button connectMyoButton = (Button) findViewById(R.id.connectMyoButton);
+        connectMyoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Connexion.this, Connexion.class);
+                startActivity(intent);
             }
         });
 
@@ -146,7 +138,6 @@ public class Connexion extends Activity {
         if (actionBar != null) {
             actionBar.hide();
         }
-        mControlsView.setVisibility(View.GONE);
         mVisible = false;
 
         // Schedule a runnable to remove the status and navigation bar after a delay
@@ -156,9 +147,7 @@ public class Connexion extends Activity {
 
     @SuppressLint("InlinedApi")
     private void show() {
-        // Show the system bar
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+
         mVisible = true;
 
         // Schedule a runnable to display UI elements after a delay
@@ -173,5 +162,16 @@ public class Connexion extends Activity {
     private void delayedHide(int delayMillis) {
         mHideHandler.removeCallbacks(mHideRunnable);
         mHideHandler.postDelayed(mHideRunnable, delayMillis);
+    }
+
+
+
+
+    public void connectSphero(){
+
+    }
+
+    public void connectMyo(){
+
     }
 }
